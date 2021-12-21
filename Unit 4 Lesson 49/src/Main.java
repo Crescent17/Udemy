c411ed9ff40fd2e8ee368d7677adc2b172bb3066
Marat <94042173+Crescent17@users.noreply.github.com>
2021-12-21 13:53:38 +0300

    Add files via upload

---
+++ Unit 4 Lesson 49/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Alex", highScorePosition);
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Mike", highScorePosition);
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("David", highScorePosition);
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Joseph", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println("Player " + name + " managed to get into position " + position);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score > 1000) {
+            return 1;
+        } else if (score > 500 && score < 1000) {
+            return 2;
+        } else if (score > 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
